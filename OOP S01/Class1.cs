5ed3d7a603c2f3d35245e8736067ccd54f2293ee
Y.C.C <Y.C.C@DESKTOP-9Q2CJID>
2025-07-19 17:27:44 +0300

    V01

---
+++ OOP S01/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_S01
+{
+    internal struct Point
+    {
+        public int x;
+        public int y;
+        //constructor: special method
+        //            1. named like struct
+        //            2.has no return type
+        //CLR: will generate the parameterless constructor
+        //constructor
+        //  public Point()
+        //  {
+
+        // }
+       public Point(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+        public void printPoint()
+        {
+            Console.WriteLine($"({x}, {y})");
+        }
+        public override string ToString()
+        {
+            return $"( {x}, {y})";
+        }
+
+
+    }
+}
--- OOP S01/Program.cs
+++ OOP S01/Program.cs
@@ -1,18 +1,40 @@
 ﻿namespace OOP_S01
 {
+    //class
+    //struct
+    //interface
+    //enum
     internal class Program
     {
         static void Main(string[] args)
         {
 
+           // Point p01;
+            //declare for object from type Point
+            //object
+            //allocate uninitilized 8 Byte at stack
+            //Console.WriteLine(p01.x);
+            // Console.WriteLine(p01.y);
+            // p01.x = 12;
+            // p01.y = 12;
+            // Console.WriteLine(p01.x);
+            // Console.WriteLine(p01.y);
 
 
+            //.Net 5.0  C# 9.0
+            //.Net 6.0  C# 10.0
+            //.Net 7.0  C# 11.0
 
 
+            //.Net 8.0  C# 12.0
+           // p01 = new Point(1,2);
+            //new:used for select the constructor
 
-
-
-
+            // Console.WriteLine(p01.x);
+            //Console.WriteLine(p01.y);
+            //  Console.WriteLine(p01);
+            // p01.printPoint();
+            //Console.WriteLine(p01.ToString()); 
 
         }
     }
