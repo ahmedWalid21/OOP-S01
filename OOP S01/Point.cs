f0ffceeefd99d60684d810c2e423ad93c76a8078
Y.C.C <Y.C.C@DESKTOP-9Q2CJID>
2025-07-19 22:39:08 +0300

    Q01

---
+++ OOP S01/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_S01
+{
+    internal class Point
+    {
+        private double x;
+        private double y;
+
+        public double X
+        {
+            set
+            {
+                this.x = value;
+            }
+            get
+            {
+                return x;
+            }
+        }
+        public double Y
+        {
+            set
+            {
+                this.y = value;
+            }
+            get
+            {
+                return y;
+            }
+        }
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+
+
+    }
+}
--- OOP S01/Program.cs
+++ OOP S01/Program.cs
@@ -1,14 +1,30 @@
-﻿namespace OOP_S01
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace OOP_S01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("enter X for Point 1: ");
+            double x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("enter Y for Point 1: ");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.Write("enter X for Point 2: ");
+            double x2 = double.Parse(Console.ReadLine());
+
+            Console.Write("enter Y for Point 2: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            Point p1 = new Point(x1, y1);
+            Point p2 = new Point(x2, y2);
 
 
-
-
-
+            double distance = Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+            Console.WriteLine($"Distance = {distance}");
 
 
 
